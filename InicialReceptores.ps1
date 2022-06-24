1175deb9ddaf629f93fe9c0257856322fabf96b1
Christian Rodriguez <christian.rodriguez@mobiik.com>
2022-06-24 18:01:41 -0500

    se elimina el deploy

--- InicialReceptores.ps1
+++ InicialReceptores.ps1
@@ -1,4 +1,4 @@
-for (($i = 0), ($j = 0); $i -lt 128; $i++)
+for (($i = 0), ($j = 0); $i -lt 10; $i++)
 {
 	invoke-expression 'cmd /c start powershell -Command { write-host "Hi, new window!"; set-location "C:\bside-projects\oxxo\poc"; python receiveEventHub.py}'
 }

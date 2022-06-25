80f3d9df9492282e129b53a56ef041ce0c3b3fe2
Christian Rodriguez <christian.rodriguez@mobiik.com>
2022-06-25 09:38:46 -0500

    Sea grega script linux

--- iniciaReceptor.sh
+++ iniciaReceptor.sh
@@ -2,4 +2,4 @@ git clone https://github.com/cdaniel1809/tickets-poc.git
 cd tickets-poc
 sudo apt-get -y install python3-pip 
 pip install -r requirements.txt
-python3 receiveEcentHub.py
+python3 receiveEventHub.py
